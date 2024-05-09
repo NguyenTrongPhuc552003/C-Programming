91037a001dd7519641e51932e8c7f65992ff629b
Hoimaly <88880664+hoimaly@users.noreply.github.com>
2024-05-09 08:37:22 +0700

    Modify main.c

--- Hello World/main.c
+++ Hello World/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-enum weekday {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY};
-
 int main() {
-    int a = MONDAY;
-    printf("%d", a);
+    printf("Hello, World!\n");
 }
